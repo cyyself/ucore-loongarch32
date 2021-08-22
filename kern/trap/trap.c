53dd4dee478d6107ac936939017760dcd3efe47e
Yangyu Chen <cyy@cyyself.name>
2021-08-23 00:16:20 +0800

    add syscall unimpl

--- kern/trap/trap.c
+++ kern/trap/trap.c
@@ -218,6 +218,7 @@ trap_dispatch(struct trapframe *tf) {
       break;
     case EX_SYS:
       tf->tf_era += 4;
+      panic("unimpl syscall");
       //syscall(); TODO: syscall
       break;
     case EX_ADE:
