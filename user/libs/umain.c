a5c4922f30f08fc29da8936e543105d2d39d080d
Yangyu Chen <cyy@cyyself.name>
2021-12-04 00:03:02 +0800

    fix sh exit

--- user/libs/umain.c
+++ user/libs/umain.c
@@ -21,7 +21,7 @@ initfd(int fd2, const char *path, uint32_t open_flags) {
 
 void
 umain(int argc, char *argv[]) {
-/*
+#ifndef PIGGY
     int fd;
     if ((fd = initfd(0, "stdin:", O_RDONLY)) < 0) {
         warn("open <stdin> failed: %e.\n", fd);
@@ -29,7 +29,7 @@ umain(int argc, char *argv[]) {
     if ((fd = initfd(1, "stdout:", O_WRONLY)) < 0) {
         warn("open <stdout> failed: %e.\n", fd);
     }
-*/
+#endif
     int ret = main(argc, argv);
     exit(ret);
 }
