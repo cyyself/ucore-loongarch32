f4da61dcfb6ba57c31f6d87005ae1cbd61e527c9
Yangyu Chen <cyy@cyyself.name>
2021-12-07 14:28:33 +0800

    fix faultreadkernel address

--- user/faultreadkernel.c
+++ user/faultreadkernel.c
@@ -3,8 +3,8 @@
 
 int
 main(void) {
-    cprintf("I read %08x from 0xa0010000u!\n", *(unsigned int*)0xa0010000u);
-    // if you tried change the address less than the end of the elf end symbol, you will fail.
+    cprintf("I read %08x from 0xaf000000!\n", *(unsigned int*)0xaf000000u);
+    // If you tried change the address less than the end of the elf end symbol, you will fail.
     // I think it's a security bug from QEMU by loongson not from uCore Kernel because it doesn't throw any exception.
     // The following code will cause kernel crash:
     //for (unsigned int *i=0xa0001000u;i<=0xa1000000;i++) *i = 0;
