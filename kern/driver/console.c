ed9d5dc7cc6e29f3e749023911b2b49e573a5865
Yangyu Chen <cyy@cyyself.name>
2022-09-17 16:34:00 +0800

    delete answers

--- kern/driver/console.c
+++ kern/driver/console.c
@@ -100,7 +100,6 @@ void serial_int_handler(void *opaque)
     int c = cons_getc();
 #if defined(LAB1_EX3) && defined(_SHOW_SERIAL_INPUT)
     // LAB1 EXERCISE3: YOUR CODE
-    kprintf("got input %c\n",c);
 #endif
 #ifdef LAB4_EX2
     extern void dev_stdin_write(char c);
